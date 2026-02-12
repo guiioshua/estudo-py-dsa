702004306e33ebdb3df3505bd95f9e0469f95fab
guiioshua <guilhermeioshua@gmail.com>
2026-02-11 21:34:30 -0300

    término método delete da hash table

--- hash_table.py
+++ hash_table.py
@@ -35,3 +35,38 @@ class HashTable:
                 return current.value[1]
             current = current.next_node_ref
         return None
+    
+    def remove(self, key):
+        slot_pos = self._get_hash_pos(key)
+        slot = self.slots[slot_pos]
+        if not slot or not slot.chain.head:
+            return None
+        
+        chain = slot.chain
+        if chain.head.value[0] == key:
+            if chain.head == chain.tail:
+                chain.tail = None
+            chain.head = chain.head.next_node_ref
+            return
+            
+        current = chain.head
+        while current.next_node_ref is not None:
+            if current.next_node_ref.value[0] == key:
+                target = current.next_node_ref
+                current.next_node_ref = target.next_node_ref
+                if target == chain.tail:
+                    chain.tail = current
+                return
+            current = current.next_node_ref
+        
+if __name__ == '__main__':
+    
+    ht = HashTable(1)
+
+    ht.put('key1', 'value1')
+    ht.put('box1', 'value2')
+    print(ht.get('key1'))
+    print(ht.get('box1'))
+    ht.remove('key1')
+    print(ht.get('key1'))
+    print(ht.get('box1'))
